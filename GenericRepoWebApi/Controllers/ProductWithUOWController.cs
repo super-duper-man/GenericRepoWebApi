30c07a01d861efd6c490e4606d058d3dc7730814
MohammadReza Fouladi <m.r.fouladi7@gmail.com>
2025-11-25 09:46:25 +0330

    create new product controller using unit of work

---
+++ GenericRepoWebApi/Controllers/ProductWithUOWController.cs
@@ -0,0 +1,71 @@
+﻿using GenericRepoWebApi.Dtos;
+using GenericRepoWebApi.Entity;
+using GenericRepoWebApi.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace GenericRepoWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductWithUOWController(IUnitOfWork unitOfWork) : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork = unitOfWork;
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            var products = await _unitOfWork.GetRepository<Product>().GetAllAsync();
+
+            return Ok(products ?? Enumerable.Empty<Product>());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct([FromBody] ProductDto product)
+        {
+            try
+            {
+                using var transaction = _unitOfWork.BeginTransactionAsync();
+                var newProduct = new Product
+                {
+                    Name = product.Name,
+                    Price = product.Price
+                };
+                var productResult = await _unitOfWork.GetRepository<Product>().AddAsync(newProduct);
+                await _unitOfWork.SaveChangesAsync();
+
+                var orderEntity = new Order
+                {
+                    OrderDate = DateTime.UtcNow,
+                    ProductId = productResult.Id
+                };
+
+                await _unitOfWork.GetRepository<Order>().AddAsync(orderEntity);
+                await _unitOfWork.SaveChangesAsync();
+
+                await _unitOfWork.CommitAsync();
+
+                return Ok(StatusCode((int)HttpStatusCode.Created, new
+                {
+                    Id = productResult.Id
+                }));
+            }
+            catch(Exception ex)
+            {
+                await _unitOfWork.RollbackAsync();
+                throw new Exception(ex.Message);
+            }
+        }
+
+        //[HttpPut]
+        //public async Task<IActionResult> UpdateProduct(ProductDto product)
+        //{
+        //}
+
+        //[HttpDelete("{productId}")]
+        //public async Task<IActionResult> DeleteProduct(int productId)
+        //{
+        //}
+    }
+}
--- GenericRepoWebApi/Program.cs
+++ GenericRepoWebApi/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
